3754fbd7530becf1969850cabcd6102ce5e47703
Hexzel <a0972180845@gmail.com>
2021-04-06 06:39:18 -0700

    make

--- 6_2_Simple_Timer/main.cpp
+++ 6_2_Simple_Timer/main.cpp
@@ -3,11 +3,17 @@
 using namespace std::chrono;
 
 Timer t;
+AnalogOut aout(PA_4);
+AnalogIn ain(PC_5);
 
 int main()
 {
    t.start();
-   printf("Hello World!\n");
+   // printf("Hello World!\n");
+   // aout.write_u16(65400);
+   // float i = 90 / 89.0 * 1.283;
+   float v_in = ain;
+   // t.elapsed_time().count();
    t.stop();
    auto s = chrono::duration_cast<chrono::seconds>(t.elapsed_time()).count();
    auto ms = chrono::duration_cast<chrono::milliseconds>(t.elapsed_time()).count();
--- 6_4_Simple_Timeout/main.cpp
+++ 6_4_Simple_Timeout/main.cpp
@@ -5,16 +5,21 @@ Timeout flipper;
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 
+volatile int lever;
 void flip()
 {
+	lever = 0;
    led2 = !led2;
 }
 
 int main()
 {
    led2 = 1;
-   flipper.attach(&flip, 2s); // setup flipper to call flip after 2 seconds
-
+   for (int i = 0; i < 10; i++) {
+   		lever = 1;
+   		flipper.attach(&flip, 2s); // setup flipper to call flip after 2 seconds
+		while (lever) ;
+	}
    // spin in a main loop. flipper will interrupt it to call flip
    while (1)
    {
